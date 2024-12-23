a396f3163bda13dedd17b721dc02279cf327e20f
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-23 17:19:26 +0800

    Refactor index.php and put it to public directory.

--- public/index.php
+++ public/index.php
@@ -1,17 +1,21 @@
 <?php
-require_once "vendor/autoload.php";
+
+namespace MainProject;
+
+require_once __DIR__ . "/../vendor/autoload.php";
 
 
 header("Content-Type:application/json");
 header("Access-Control-Add-Origin:*");
 
+$viewsDir = "/../src/MainProject/views/";
 $request = $_SERVER["REQUEST_URI"];
 switch ($request) {
 	case '/':
-		require __DIR__ . "/views/index.php";
+		require __DIR__ . $viewsDir . "home.php";
 		break;
 	case '/token':
-		require __DIR__ . "/views/token.php";
+		require __DIR__ . $viewsDir . "token.php";
 		break;
 	default:
 		http_response_code(404);
