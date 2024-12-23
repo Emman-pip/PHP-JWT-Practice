69f3ab1b4274d1cea5e90eeae9b1ef3805e62d9c
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-23 16:19:00 +0800

    Fix routing bug.
    Add a error 404 and break statements to the switch cases.

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 require_once "vendor/autoload.php";
 
-$request = $_SERVER["REQUEST_URI"];
 
 header("Content-Type:application/json");
 header("Access-Control-Add-Origin:*");
 
+$request = $_SERVER["REQUEST_URI"];
 switch ($request) {
+	case '/':
+		require __DIR__ . "/views/index.php";
+		break;
 	case '/token':
 		require __DIR__ . "/views/token.php";
+		break;
 	default:
-		echo json_encode(["message" => "Welcome to php-sample-api!!!"]);
+		http_response_code(404);
+		break;
 }
