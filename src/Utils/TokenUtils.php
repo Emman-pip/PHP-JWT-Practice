917a2cd90f0d565110d553aae1f7c1877c993590
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-24 14:37:36 +0800

    Add verify token method.

--- src/Utils/TokenUtils.php
+++ src/Utils/TokenUtils.php
@@ -12,7 +12,7 @@ class TokenUtils
 	public static function generateToken($key, $payload) # returns a string
 	{
 		$jwt = JWT::encode($payload, $key, "HS256", null, null);
-		list($headersB64, $payloadB64, $sig) = explode(".", $jwt);
+		/*list($headersB64, $payloadB64, $sig) = explode(".", $jwt);*/
 		return $jwt;
 	}
 
@@ -21,6 +21,19 @@ class TokenUtils
 	{
 		list($headersB64, $payloadB64, $sig) = explode(".", $jwt);
 		$decoded = base64_decode($payloadB64);
-		return $decoded;
+		return json_decode($decoded, true);
+	}
+
+	public static function verifyToken($jwt)
+	{
+		$env = parse_ini_file(__DIR__ . "/../../.env");
+		$username = TokenUtils::payloadDecoder($jwt)["username"];
+		$userData = Database::query("SELECT username, password from Users where username='" . $username . "';")->fetch();
+		$regeneratedToken = TokenUtils::generateToken($env["SECRETKEY"], ["username" => $userData["0"], "password" => $userData["1"]]);
+		if ($regeneratedToken != $jwt) {
+			return false;
+		}
+		return true;
 	}
 }
+// (evil-local-mode)
