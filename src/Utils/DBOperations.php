0713e8a71649d8199a988c2cb6c18b54012dec30
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-25 08:23:02 +0800

    Create tasks specific databasef operations.

--- src/Utils/DBOperations.php
+++ src/Utils/DBOperations.php
@@ -13,9 +13,23 @@ class DBOperations
 	}
 	public static function insertTasks($taskName, $description, $username)
 	{
-		$date = new DateTime();
-		Database::executeQuery(sprintf("INSERT INTO Tasks(taskName, description, lastUpdated, username) VALUES ('%s', '%s', '%s', '%s')", $taskName, $description, $date, $username));
+		$date = date('Y-m-d H:i:s');
+
+		Database::executeQuery(sprintf("INSERT INTO Tasks(taskName, description, lastUpdated, owner) VALUES ('%s', '%s', '%s', '%s')", $taskName, $description, $date, $username));
 	}
+
+	public static function updateTasks($taskId, $taskName, $description)
+	{
+		$date = date('Y-m-d H:i:s');
+		Database::executeQuery(sprintf("UPDATE Tasks set taskName='%s', description='%s', lastUpdated='%s' where taskId=%d;", $taskName, $description, $date, $taskId));
+	}
+
+
+	public static function deleteTasks($taskId)
+	{
+		Database::executeQuery(sprintf("delete from Tasks where taskId=%d", $taskId));
+	}
+
 	public static function getUserTasks($username)
 	{
 		return Database::query(sprintf("Select * from Tasks where owner='%s' ORDER BY lastUpdated ASC", $username));
