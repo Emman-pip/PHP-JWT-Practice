d5b391dc4762d56cafb37f4d29343fe4d47de4c7
Emman-pip <parrenoemmanuel756@gmail.com>
2024-12-25 08:22:21 +0800

    Add routes for tasks.
    Add routes for the following:
      new tasks
      update tasks
      delete tasks

--- public/index.php
+++ public/index.php
@@ -2,9 +2,10 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-
 header("Content-Type:application/json");
 header("Access-Control-Add-Origin:*");
+
+use MainProject\Utils\TaskOperations;
 
 $viewsDir = "/../src/Views/";
 $request = $_SERVER["REQUEST_URI"];
@@ -14,17 +15,29 @@ switch ($request) {
 		break;
 	case '/user-log':
 		require __DIR__ . $viewsDir . "log.php";
-        break;
-    case '/tasks':
-        require __DIR__ . $viewsDir . "tasks.php";
-        break;
-    case '/user-new':
-        require __DIR__ . $viewsDir . "new.php";
-        break;
+		break;
+	case '/tasks':
+		require __DIR__ . $viewsDir . "tasks.php";
+		break;
+	case '/user-new':
+		require __DIR__ . $viewsDir . "new.php";
+		break;
+	case '/task-new':
+		/*this requires a post request, token, and post variables (taskName, description)	*/
+		$task = new TaskOperations();
+		$task->newTask();
+		break;
+	case '/task-update':
+		/*this requires a post request, token, and post variables (taskId, taskName, description)	*/
+		$task = new TaskOperations();
+		$task->updateTask();
+		break;
+	case '/task-delete':
+		/*this requires a post request, token, and post variables (taskId)	*/
+		$task = new TaskOperations();
+		$task->deleteTask();
+		break;
 	default:
 		http_response_code(404);
 		break;
 }
-
-
-
